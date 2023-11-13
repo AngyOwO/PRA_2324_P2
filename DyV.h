d5f9429065449714ed72f5a0b33ec3f9b2c1d4ba
AngyOwO <espertcornejoangela@gmail.com>
2023-11-13 19:45:47 +0100

    Añadida implementación Divide y Vencerás

--- DyV.h
+++ DyV.h
@@ -3,23 +3,51 @@
 
 #include <vector>
 #include <iostream>
+#include <ostream>
 
 template <typename T>
 
-T BusquedaBinaria(T x, std::vector<T>& v, int ini, int fin){
+T BusquedaBinaria (T x, std::vector<T>& v, int ini, int fin){
 
-	if(ini > fin)
+	if (ini > fin){
 		std::cout << "No se encuentra en el array" << std::endl;	
+		return -1;
+	}
 
 	int medio = (ini + fin) / 2;
 
-	if(v[medio] == x)
+	if (v[medio] == x)
 		return medio;
-	else if(v[medio] > x)
+	else if (v[medio] > x)
 		return BusquedaBinaria(x, v, ini, medio - 1);
 	else
 		return BusquedaBinaria(x, v, medio + 1, fin);
 
+}
+
+int Partition (std::vector<T>& v, int ini, int fin){
+
+        T x = v[fin];
+        int i = ini;
+
+        for (int j = ini; j <= fin; j++){
+                if (v[j] <= x){
+                        v[i] = v[j];
+                        i = i + 1;
+                }
+        }
+
+        v[i] = v[fin];
+        return i;
+}
+
+void QuickSort (std::vector<T>& v, int ini, int fin){
+	
+	if (ini < fin){
+		int pivot = Partition(v, ini, fin);
+		QuickSort(v, ini, pivot - 1);
+		QuickSort(v, pivot + 1, fin);
+	}
 }
 
 #endif
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,9 +1,30 @@
 #include "DyV.h"
+#include <iostream>
+#include <chrono>
+
 
 int main(){
 	std::vector<int> a{1,2,3,4,5,6,7,8,9};
 	std::vector<double> b{1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0};
 
-	BusquedaBinaria(2,a,0,8);
-	BusquedaBinaria(4.0,b,0,8);
+	std::cout << BusquedaBinaria(2,a,0,8) << std::endl;
+	std::cout << BusquedaBinaria(4.0,b,0,8) << std::endl;
+
+	std::vector<int> c{12,45,21,35,3,56,13,22,9,10};
+
+	auto = start = std::chrono::system_clock::now();
+	QuickSort(c,0,9);
+	auto end = std::chrono::system_clock::now();
+
+	std::chrono::duration<float, std::milli> duration = end - start;
+
+	std::cout << "Time: " << duration.count() << "s" << std::endl;
+
+	std::cout << "[";
+    	for (const auto& elem : c) {
+		std::cout << elem << " ";
+    	}
+	std::cout << "]" << endl;
+
+	return 0;
 }
