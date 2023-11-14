91b3212172c13489740955692956412cc1eabf42
AngyOwO <espertcornejoangela@gmail.com>
2023-11-14 10:56:29 +0100

    Implementación Divide y Vencerás finalizada

--- DyV.h
+++ DyV.h
@@ -5,6 +5,11 @@
 #include <iostream>
 #include <ostream>
 
+#include <cstdlib>
+#include <ctime>
+
+/*------------------------- BUSQUEDA BINARIA -----------------------------*/
+
 template <typename T>
 
 T BusquedaBinaria (T x, std::vector<T>& v, int ini, int fin){
@@ -25,29 +30,199 @@ T BusquedaBinaria (T x, std::vector<T>& v, int ini, int fin){
 
 }
 
-int Partition (std::vector<T>& v, int ini, int fin){
+/*************************************************************************/
+/*************************************************************************/
 
-        T x = v[fin];
+/*~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCIONES DE AYUDA ~~~~~~~~~~~~~~~~~~~~~~~~~*/
+
+//INTERCAMBIAR
+
+template <typename W>
+
+void intercambiar(std::vector<W>& v, int i, int j){
+
+	W aux = v[i];
+	v[i] = v[j];
+	v[j] = aux;
+}
+
+//IMPRIMIR
+
+template <typename X>
+
+void print(std::vector<X>& v, int size){
+
+	for(int i = 0; i < size; i++){
+		std::cout<< " " << v[i] << std::endl;
+	}
+
+}
+
+/*------------------------------ PARTITION ------------------------------*/
+
+//ÚLTIMO ELEMENTO
+
+template <typename U>
+
+int Partition_ultElem (std::vector<U>& v, int ini, int fin){
+
+        U x = v[fin];
         int i = ini;
 
-        for (int j = ini; j <= fin; j++){
+        for (int j = ini; j < fin; j++){
                 if (v[j] <= x){
-                        v[i] = v[j];
-                        i = i + 1;
+                        intercambiar(v, i, j);
+                        i++;
                 }
         }
 
-        v[i] = v[fin];
+        intercambiar(v, i, fin);
+
         return i;
 }
 
-void QuickSort (std::vector<T>& v, int ini, int fin){
+//PRIMER ELEMENTO
+
+template <typename Y>
+
+int Partition_primElem (std::vector<Y>& v, int ini, int fin){
+
+	Y x = v[ini];
+	int i = ini;
+
+	for (int j = ini + 1; j <= fin; j++){
+
+		if (v[j] <= x){
+			i++;
+			intercambiar(v, i, j);
+		}
+	}
+
+	intercambiar(v, i, ini);
+
+	return i;
+}
+
+//ELEMENTO CENTRAL
+
+template <typename A>
+
+int Partition_midElem (std::vector<A>& v, int ini, int fin){
+
+	int mid = (ini + fin) / 2;
+	A x = v[mid];
+
+	int i = ini, j = fin;
+
+	while (i <= j){
+
+		while (v[i] < x){
+			i++;
+		}
+
+		while (v[j] > x){
+			j--;
+		}
+
+		if (i <= j){
+			intercambiar(v, i, j);
+			i++;
+			j--;
+		}
+
+	}
+
+	return i;
+}
+
+//ELEMENTO ALEATORIO
+
+template <typename C>
+
+int Partition_randElem (std::vector<C>& v, int ini, int fin){
+
+	srand( time(nullptr) );
+
+	C x = v[ini + rand() % (fin - ini + 1)];
+
+	int i = ini, j = fin;
+
+	while (i < j){
+
+        	while (v[i] < x){
+            		i++;
+		}
+
+        	while (v[j] > x){
+            		j--;
+		}
+
+        	if (i < j){
+			intercambiar(v, i, j);
+		}
+
+    	}
+
+    	return j;
+
+}
+
+
+
+/*------------------------------ QUICKSORT -------------------------------*/
+
+//ÚLTIMO ELEMENTO
+
+template <typename V>
+
+void QuickSort_ultElem (std::vector<V>& v, int ini, int fin){
 	
 	if (ini < fin){
-		int pivot = Partition(v, ini, fin);
-		QuickSort(v, ini, pivot - 1);
-		QuickSort(v, pivot + 1, fin);
+		int pivot = Partition_ultElem(v, ini, fin);
+		QuickSort_ultElem(v, ini, pivot - 1);
+		QuickSort_ultElem(v, pivot + 1, fin);
 	}
 }
+
+//PRIMER ELEMENTO
+
+template <typename Z>
+
+void QuickSort_primElem (std::vector<Z>& v, int ini, int fin){
+
+	if (ini < fin){
+                int pivot = Partition_primElem(v, ini, fin);
+                QuickSort_primElem(v, ini, pivot - 1);
+                QuickSort_primElem(v, pivot + 1, fin);
+        }
+
+}
+
+//ELEMENTO CENTRAL
+
+template <typename B>
+
+void QuickSort_midElem (std::vector<B>& v, int ini, int fin){
+
+	if (ini < fin){
+		int pivot = Partition_midElem(v, ini, fin);
+                QuickSort_midElem(v, ini, pivot - 1);
+                QuickSort_midElem(v, pivot, fin);
+	}
+}
+
+//ELEMENTO ALEATORIO
+
+template <typename D>
+
+void QuickSort_randElem (std::vector<D>& v, int ini, int fin){
+
+        if (ini < fin){
+                int pivot = Partition_randElem(v, ini, fin);
+                QuickSort_randElem(v, ini, pivot - 1);
+                QuickSort_randElem(v, pivot + 1, fin);
+        }
+}
+
 
 #endif
--- test_DyV.cpp
+++ test_DyV.cpp
@@ -1,30 +1,301 @@
 #include "DyV.h"
-#include <iostream>
 #include <chrono>
 
+using namespace std;
 
-int main(){
-	std::vector<int> a{1,2,3,4,5,6,7,8,9};
-	std::vector<double> b{1.0,2.0,3.0,4.0,5.0,6.0,7.0,8.0,9.0};
+void Inicializar_variables(vector<int> &v_int, vector<double> v_double, vector<char> v_char){
 
-	std::cout << BusquedaBinaria(2,a,0,8) << std::endl;
-	std::cout << BusquedaBinaria(4.0,b,0,8) << std::endl;
+	v_int = {80, 52, 9, 21};
+	v_double = {83.4, 5.3, 23.2, 51.6};
+	v_char = {'h', 's', 'v', 'b'};
 
-	std::vector<int> c{12,45,21,35,3,56,13,22,9,10};
+}
 
-	auto = start = std::chrono::system_clock::now();
-	QuickSort(c,0,9);
+void Medicion_Tiempos(){
+
+	vector<int> v_int;
+	vector<double> v_double;
+	vector<char> v_char;
+
+	Inicializar_variables(v_int, v_double, v_char);
+
+	int size_int = v_int.size();
+	int size_double = v_double.size();
+	int size_char = v_char.size();	
+
+	//// ÚLTIMO ////
+
+	auto start = std::chrono::system_clock::now();
+	QuickSort_ultElem(v_int, 0, size_int - 1);
 	auto end = std::chrono::system_clock::now();
-
 	std::chrono::duration<float, std::milli> duration = end - start;
+	std::cout << "Time QuickSort último elemento int: " << duration.count() << "s" << std::endl;
 
-	std::cout << "Time: " << duration.count() << "s" << std::endl;
+	start = std::chrono::system_clock::now();
+	QuickSort_ultElem(v_double, 0, size_double - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort último elemento double: " << duration.count() << "s" << std::endl;
 
-	std::cout << "[";
-    	for (const auto& elem : c) {
-		std::cout << elem << " ";
-    	}
-	std::cout << "]" << endl;
+	start = std::chrono::system_clock::now();
+	QuickSort_ultElem(v_char, 0, size_char - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort último elemento char: " << duration.count() << "s" << std::endl;
+
+	//// PRIMERO ////
+
+	start = std::chrono::system_clock::now();
+	QuickSort_primElem(v_int, 0, size_int - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort primer elemento int: " << duration.count() << "s" << std::endl;
+
+	start = std::chrono::system_clock::now();
+	QuickSort_primElem(v_double, 0, size_double - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort primer elemento double: " << duration.count() << "s" << std::endl;
+
+	start = std::chrono::system_clock::now();
+	QuickSort_primElem(v_char, 0, size_char - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort primer elemento char: " << duration.count() << "s" << std::endl;
+
+	//// CENTRAL ////
+
+	start = std::chrono::system_clock::now();
+	QuickSort_midElem(v_int, 0, size_int - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort elemento central int: " << duration.count() << "s" << std::endl;
+
+	start = std::chrono::system_clock::now();
+	QuickSort_midElem(v_double, 0, size_double - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort elemento central double: " << duration.count() << "s" << std::endl;
+
+	start = std::chrono::system_clock::now();
+	QuickSort_midElem(v_char, 0, size_char - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort elemento central char: " << duration.count() << "s" << std::endl;
+
+	//// ALEATORIO ////
+
+	start = std::chrono::system_clock::now();
+	QuickSort_randElem(v_int, 0, size_int - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort elemento aleatorio int: " << duration.count() << "s" << std::endl;
+
+	start = std::chrono::system_clock::now();
+	QuickSort_randElem(v_double, 0, size_double - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort elemento aleatorio double: " << duration.count() << "s" << std::endl;
+
+	start = std::chrono::system_clock::now();
+	QuickSort_randElem(v_char, 0, size_char - 1);
+	end = std::chrono::system_clock::now();
+	duration = end - start;
+	std::cout << "Time QuickSort elemento aleatorio char: " << duration.count() << "s" << std::endl;
+
+}
+
+int main(int argc, char** argv){
+	
+	//// BÚSQUEDA BINARIA ////
+
+	cout << "		>> " << "BÚSQUEDA BINARIA" << " <<		" << endl << endl;
+
+	vector<int> v_int = {8, 15, 23, 51};
+	int x_int = 8;
+	int size_int = v_int.size();
+	
+	vector<double> v_double = {8.4, 15.3, 23.2, 51.6};
+	double x_double = 23.2;
+	int size_double = v_double.size();
+	
+	vector<char> v_char = {'a', 'e', 'h', 'v'};
+	char x_char = 'v';
+	int size_char = v_char.size();	
+	
+	cout << "Vector original int:";
+	print(v_int, size_int);
+	cout << endl << endl;
+	
+	cout << "Se va a buscar el número " << x_int << " dentro del vector y se devolverá su posición." << endl << endl;
+	
+	cout << "Búsqueda binaria: " << BusquedaBinaria(x_int, v_int, 0, size_int - 1) << endl << endl;
+	
+	cout << "Vector original double:";
+	print(v_double, size_double);
+	cout << endl << endl;
+		
+	cout << "Se va a buscar el número " << x_double << " dentro del vector y se devolverá su posición." << endl << endl;
+		
+	cout << "Búsqueda binaria: " << BusquedaBinaria(x_double, v_double, 0, size_double - 1) << endl << endl;
+	
+	cout << "Vector original char:";
+	print(v_char, size_char);
+	cout << endl << endl;
+	
+	cout << "Se va a buscar la letra " << x_char << " dentro del vector y se devolverá su posición." << endl << endl;
+	
+	cout << "Búsqueda binaria: " << BusquedaBinaria(x_char, v_char, 0, size_char - 1) << endl << endl;
+
+	//// QUICK SORT ÚLTIMO ////
+
+	cout << "		>> " << "QUICK SORT ÚLTIMO" << " <<		" << endl << endl;
+	
+	//inicializamos las variables con valores desordenados y manteniendo el tamaño
+	Inicializar_variables(v_int, v_double, v_char);
+	
+	cout << "Se va a ordenar el vector: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	QuickSort_ultElem(v_int, 0, size_int - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	cout << "Se va a ordenar el vector: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+
+	QuickSort_ultElem(v_double, 0, size_double - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+		
+	cout << "Se va a ordenar el vector: ";
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	QuickSort_ultElem(v_char, 0, size_char - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	//// QUICK SORT PRIMERO ////
+
+	cout << "		>> " << "QUICK SORT PRIMERO" << " <<		" << endl << endl;
+	
+	Inicializar_variables(v_int, v_double, v_char);
+	
+	cout << "Se va a ordenar el vector: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	QuickSort_primElem(v_int, 0, size_int - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	cout << "Se va a ordenar el vector: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+
+	QuickSort_primElem(v_double, 0, size_double - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+		
+	cout << "Se va a ordenar el vector: ";
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	QuickSort_primElem(v_char, 0, size_char - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	//// QUICK SORT CENTRAL ////
+
+	cout << "		>> " << "QUICK SORT CENTRAL" << " <<		" << endl << endl;
+	
+	Inicializar_variables(v_int, v_double, v_char);
+	
+	cout << "Se va a ordenar el vector: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	QuickSort_midElem(v_int, 0, size_int - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	cout << "Se va a ordenar el vector: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+
+	QuickSort_midElem(v_double, 0, size_double - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+		
+	cout << "Se va a ordenar el vector: ";
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	QuickSort_midElem(v_char, 0, size_char - 1);
+
+	cout << "Vector ordenado: "; 
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	//// QUICK SORT ALEATORIO ////
+
+	cout << "		>> " << "QUICK SORT ALEATORIO" << " <<		" << endl << endl;
+	
+	Inicializar_variables(v_int, v_double, v_char);
+
+	cout << "Se va a ordenar el vector: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	QuickSort_randElem(v_int, 0, size_int - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_int, size_int);
+	cout << endl << endl;
+
+	cout << "Se va a ordenar el vector: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+
+	QuickSort_randElem(v_double, 0, size_double - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_double, size_double);
+	cout << endl << endl;
+		
+	cout << "Se va a ordenar el vector: ";
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	QuickSort_randElem(v_char, 0, size_char - 1);
+
+	cout << "Vector ordenado: ";
+	print(v_char, size_char);
+	cout << endl << endl;
+
+	cout << "		>> " << "MEDICIÓN DE TIEMPOS" << " <<		" << endl << endl;
+
+	Medicion_Tiempos();
 
 	return 0;
+
 }
